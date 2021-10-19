694303f32e206cfb1926c7d65ebacc8ed1bc4766
Edgar Gharibyan <edgharibyan1@gmail.com>
2021-10-19 20:43:44 +0400

    v4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const perfumeTopItemBlock = document.querySelector('.perfume-top__item-block');
 const cosmeticsFilters = document.querySelector('.cosmetics-filters');
 const burgerMenu = document.querySelector('.burger-menu');
-const mobileMenuList = document.querySelector('.mobile-menu__list');
+const mobileMenuItem = document.querySelector('.mobile-menu__item');
+const mobileMenu = document.querySelector('.mobile-menu')
+const headerNavShopping = document.querySelector('.header-nav__shopping');
+const headerBaskek = document.querySelector('.header-baskek');
+const headerBaskekTopSpan = document.querySelector('.header-baskek__top span');
+const headerItem = document.querySelector('.header-item');
 // const cosmeticsType = document.querySelector('.cosmetics-type1');
 // const brandsType = document.querySelector('.cosmetics-type2');
 
@@ -11,10 +16,21 @@ const mobileMenuList = document.querySelector('.mobile-menu__list');
 
 // for()
 
+
+headerBaskekTopSpan.addEventListener('click', () =>{
+    headerBaskek.classList.toggle('active')
+})
+
+headerNavShopping.addEventListener('click', () => {
+    headerBaskek.classList.toggle('active')
+})
+
 burgerMenu.addEventListener('click', event => {
-    mobileMenuList.classList.toggle('active');
+    mobileMenuItem.classList.toggle('active');
+    mobileMenu.classList.toggle('active')
     document.querySelector('.overlay').classList.toggle('active')
     document.querySelector('body').classList.toggle('active')
+    headerItem.classList.toggle('active')
 })
 
 cosmeticsFilters.addEventListener('click', event => {
@@ -27,3 +43,16 @@ cosmeticsFilters.addEventListener('click', event => {
 perfumeTopItemBlock.addEventListener('click', event => {
     event.target.classList.toggle('active')
 })
+
+const anchors = document.querySelectorAll('a[href*="#"');
+
+for(let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
